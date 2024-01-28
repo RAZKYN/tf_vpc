ab239c350209336f3b00668df02ea5bdce577dfa
Razkyn <razkyn@gmail.com>
2024-01-28 21:01:18 +0530

    added changes

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# install jenkins
-
+# Install jenkins
 sudo su -
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
@@ -10,17 +9,15 @@ sudo yum install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
 
-# install git
+# Install git
 sudo yum install git -y
 
-# install terraform
-
+# Install Terraform
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 
-# install kubectl
-
+# Install kubectl
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
